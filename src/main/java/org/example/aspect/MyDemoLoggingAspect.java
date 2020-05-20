4d77fbff0517492d9a0d31793cea32d5bd16464f
nilay <mailnilaydas@gmail.com>
2020-05-20 12:28:44 +0530

    Initial commit

---
+++ src/main/java/org/example/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,28 @@
+package org.example.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+
+
+	@Before("execution(public void addAccount())")
+	public void beforeAddAccountAdvice() {
+		
+		System.out.println("\n=====>>> Executing @Before advice on addAccount()");
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
